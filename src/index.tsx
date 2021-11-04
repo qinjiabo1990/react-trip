9c604b6e935b5a44c8e6cc193f9a7026674348de
Bob Qin <bob.qin@tafeqld.edu.au>
2021-11-04 17:12:38 +1000

    redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,17 @@ import App from './App';
 import axios from 'axios'; 
 import './i18n/configs';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import rootStore from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 
 axios.defaults.headers['x-icode'] = 'BB9091AEAC3502C6';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
+		<Provider store={rootStore.store}>
+			<PersistGate loading={<div>Loading</div>} persistor={rootStore.persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -8,7 +8,14 @@ import { productDetailSlice } from "./productDetail/slice";
 import { productSearchSlice } from "./productSearch/slice";
 import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { userSlice } from "./user/slice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist:['user']
+}
 
 const rootReducer = combineReducers({
 	language: languageReducer,
@@ -18,13 +25,17 @@ const rootReducer = combineReducers({
 	user: userSlice.reducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 //const store = createStore(rootReducer, applyMiddleware(thunk, actionLog, languageChangeMiddle));
 const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
 	middleware: (getDefaultMiddleware)=>[...getDefaultMiddleware(), actionLog],
 	devTools: true,
 })
 
+const persistor = persistStore(store)
+
 export type RootState = ReturnType<typeof store.getState>
 
-export default store;
+export default {store, persistor};
